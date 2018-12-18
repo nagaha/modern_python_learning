157da3905c0cffb3a9046db933e17509cdf00ccc
juakashi <juakashi@cisco.com>
2018-12-18 00:45:07 -0500

    afdsafsad

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
-aaaaa
+#!/usr/bin/python3.6
+#aaaaaabbbbbb
+print("hellow world!")
+
+x = 14
+print('the answer is %d' % x)
+x=16
+print('the answer is {}'.format(x))
+x=19
+print(f'the answer is {x}')
